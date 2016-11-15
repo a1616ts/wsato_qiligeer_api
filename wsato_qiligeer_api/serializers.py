e01878d834b55a1cc119c1c67cc3b8da99bfb335
a1616ts <a1616ts@aiit.ac.jp>
2016-11-15 15:20:02 +0900

    Modify serializer.

--- wsato_qiligeer_api/serializers.py
+++ wsato_qiligeer_api/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 
 
-class VmSerializer(serializers.Serializer):
+class StatusSerializer(serializers.Serializer):
     status = serializers.CharField(max_length=256, required=False)
+
+class ResultSerializer(serializers.Serializer):
+    result = serializers.CharField(max_length=256, required=False)
