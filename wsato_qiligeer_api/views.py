7b560cfdcde2ef3be3d92cfb2b9f3f01db63fb3c
a1616ts <a1616ts@aiit.ac.jp>
2016-11-16 13:55:41 +0900

    Add delete method.

--- wsato_qiligeer_api/views.py
+++ wsato_qiligeer_api/views.py
@@ -16,7 +16,6 @@ class Vm(APIView):
         if display_name == None or user_id == None:
             raise exceptions.ValidationError(detail = None)
 
-        # TODO Should we use Django's model?
         db = dataset.connect('mysql://api_user:apiUser@1115@127.0.0.1/wsato_qiligeer')
         table = db['domains']
         results = table.find_one(display_name = display_name, user_id = user_id)
@@ -39,7 +38,6 @@ class Vm(APIView):
         if display_name == None or user_id == None:
             raise exceptions.ValidationError(detail = None)
 
-        # TODO Validation
         db = dataset.connect('mysql://api_user:apiUser@1115@127.0.0.1/wsato_qiligeer')
         table = db['domains']
         results = table.find_one(display_name = display_name, user_id = user_id)
@@ -70,10 +68,45 @@ class Vm(APIView):
 
     def put(self, request, format = None):
         # TODO
-        # Receive suspend and resume, close
+        # Receive suspend and resume
         # Enqueue to from_api_to_middleware
         pass
 
     def delete(self, request, format = None):
-        # TODO
-        pass
+        request_get = request.data
+        display_name = request_get.get('name')
+        user_id = request_get.get('user_id')
+
+        if display_name == None or user_id == None:
+            raise exceptions.ValidationError(detail = None)
+
+        db = dataset.connect('mysql://api_user:apiUser@1115@127.0.0.1/wsato_qiligeer')
+        table = db['domains']
+        results = table.find_one(display_name = display_name, user_id = user_id)
+        if results == None :
+            return Response(status = status.HTTP_404_NOT_FOUND)
+
+        if results['status'] == 'destroy':
+            return Response(status = status.HTTP_406_NOT_ACCEPTABLE)
+
+        credentials = pika.PlainCredentials('server1_api', '34FS1Ajkns')
+        connection  = pika.BlockingConnection(pika.ConnectionParameters(
+                virtual_host = '/server1', credentials = credentials))
+        channel = connection.channel()
+
+        channel.queue_declare(queue = 'from_api_to_middleware', durable = True)
+        properties = pika.BasicProperties(
+                content_type = 'text/plain',
+                delivery_mode = 2)
+
+        enqueue_message = {
+            'ope'          : 'destroy',
+            'user_id'      : user_id,
+            'display_name' : display_name
+        }
+
+        channel.basic_publish(exchange = '',
+                              routing_key = 'from_api_to_middleware',
+                              body = json.dumps(enqueue_message))
+        connection.close()
+        return Response(status = status.HTTP_202_ACCEPTED)
