0e78e57897091518c6e6ea7dfb649c2ad3cf78bc
a1616ts <a1616ts@aiit.ac.jp>
2016-12-06 20:03:28 +0900

    Modify.

--- wsato_qiligeer_api/models.py
+++ wsato_qiligeer_api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Domains(models.Model):
     """ドメイン"""
     display_name = models.CharField('instance name', max_length=45)
+    os = models.CharField('os', max_length=255, blank=False)
     user_id = models.IntegerField('user id')
     server_id = models.IntegerField('serever id')
     size = models.IntegerField('size')
@@ -21,10 +22,9 @@ class Domains(models.Model):
         return self.name
 
 class VcServers(models.Model):
-    """ドメイン"""
-    id = models.IntegerField('名前', max_length=45)
-    name = models.CharField('名前', max_length=45)
-    free_size_gb = models.IntegerField('名前', max_length=45)
+    id = models.IntegerField(max_length=45)
+    name = models.CharField(max_length=45)
+    free_size_gb = models.IntegerField()
     create_date = models.DateField()
     update_date = models.DateField()
     class Meta:
--- wsato_qiligeer_api/views.py
+++ wsato_qiligeer_api/views.py
@@ -16,7 +16,7 @@ class Vm(APIView):
         display_name = request_get.get('name')
         user_id = request_get.get('user_id')
         if user_id == None:
-            raise Response(status = status.HTTP_403_FORBIDDEN)
+            return Response(status = status.HTTP_403_FORBIDDEN)
 
         if display_name == None:
             domains = Domains.objects.filter(user_id = user_id)
@@ -29,14 +29,16 @@ class Vm(APIView):
                     results.append({
                         'id' : count,
                         'name': domain.display_name,
+                        'os': domain.os,
                         'status': domain.status,
                         'size': domain.size,
                         'ram': domain.ram,
                         'vcpus': domain.vcpus,
-                        'ip': domain.ipv4_address,
-                        'key_path': domain.sshkey_path,
+                        'ipv4_address': domain.ipv4_address,
+                        'sshkey_path': domain.sshkey_path,
+                        'create_date': domain.create_date,
+                        'update_date': domain.update_date,
                     })
-                    print(results)
                     count = count + 1
                 return Response(results)
 
@@ -47,60 +49,79 @@ class Vm(APIView):
             else:
                 return Response({
                     'name': domain.display_name,
+                    'os': domain.os,
                     'status': domain.status,
                     'size': domain.size,
                     'ram': domain.ram,
                     'vcpus': domain.vcpus,
-                    'ip': domain.ip,
-                    'key_path': domain.key_path,
+                    'ipv4_address': domain.ipv4_address,
+                    'sshkey_path': domain.sshkey_path,
+                    'create_date': domain.create_date,
+                    'update_date': domain.update_date,
                 })
 
     def post(self, request, format = None):
         request_get = request.data
-
-        display_name = request_get.get('name')
+        name = request_get.get('name')
         user_id = request_get.get('user_id')
         size = request_get.get('size')
         ram = request_get.get('ram')
         vcpus = request_get.get('vcpus')
-
-        if display_name == None or user_id == None:
+        os = request_get.get('os')
+        if name == None or user_id == None:
             raise exceptions.ValidationError(detail = None)
 
-        domain = Domains.objects.filter(display_name = display_name, user_id = user_id)[0]
+        domain = Domains.objects.filter(display_name = name, user_id = user_id)[0]
         if domain != None :
-            return Response(status = status.HTTP_403_FORBIDDEN)
+           return Response(status = status.HTTP_403_FORBIDDEN)
 
         if size == None:
             size = 10
         else:
             size = int(size)
 
-        vc_server = VcServers.objects.order_by('-free_size_gb')[0]
-        free_size_gb = int(vc_server['free_size_gb'])
+        if ram == None:
+            ram = 1024
+        else:
+            ram = int(ram)
 
-        if free_size_gb + size < size:
-            return Response(status = status.HTTP_503_SERVICE_UNAVAILABLE)
+        if vcpus == None:
+            vcpus = 1
+        else:
+            vcpus = int(vcpus)
 
-        credentials = pika.PlainCredentials('server1_api', '34FS1Ajkns')
+        target_server = None
+        t_size = 0
+        t_core = 0
+        t_ram = 0
+        for server in VcServers.objects:
+            t_size = int(vc_server.free_size_gb) - size
+            t_core = int(vc_server.free_cpu_core) - vcpus
+            t_ram = int(vc_server.free_memory_byte) - ram
+            if 0 < t_size and  0 < t_core and 0 < t_ram:
+                target_server = server
+
+        if target_server == None
+             return Response(status = status.HTTP_503_SERVICE_UNAVAILABLE)
+
+        credentials = pika.PlainCredentials('server1_dcm', '8nfdsS12gaf')
         connection  = pika.BlockingConnection(pika.ConnectionParameters(
                 virtual_host = '/server1', credentials = credentials))
         channel = connection.channel()
-
         channel.queue_declare(queue = 'from_api_to_middleware', durable = True)
         properties = pika.BasicProperties(
                 content_type = 'text/plain',
                 delivery_mode = 2)
 
         enqueue_message = {
-            'ope'          : 'create',
+            'op'          : 'create',
             'user_id'      : user_id,
-            'name' : name,
+            'name'         : name,
             'size'         : size,
             'ram'          : ram,
-            'vcpus'        : vcpus
+            'vcpus'        : vcpus,
+            'os'           : os
         }
-
         channel.basic_publish(exchange = '',
                               routing_key = 'from_api_to_middleware',
                               body = json.dumps(enqueue_message))
@@ -109,14 +130,14 @@ class Vm(APIView):
 
     def put(self, request, format = None):
         request_get = request.data
-        display_name = request_get.get('name')
+        name = request_get.get('name')
         user_id = request_get.get('user_id')
         op = request_get.get('op')
 
-        if display_name == None or user_id == None or op == None:
+        if name == None or user_id == None or op == None:
             raise exceptions.ValidationError(detail = None)
 
-        domain = Domains.objects.filter(display_name = display_name, user_id = user_id)[0]
+        domain = Domains.objects.filter(display_name = name, user_id = user_id)[0]
         if domain == None :
             return Response(status = status.HTTP_404_NOT_FOUND)
 
@@ -137,22 +158,24 @@ class Vm(APIView):
                 delivery_mode = 2)
 
         enqueue_message = {
-            'op'          : op,
-            'user_id'      : user_id,
-            'display_name' : display_name
+            'op'      : op,
+            'user_id' : user_id,
+            'name'    : name
         }
 
         channel.basic_publish(exchange = '',
                               routing_key = 'from_api_to_middleware',
                               body = json.dumps(enqueue_message))
+
         connection.close()
         return Response(status = status.HTTP_202_ACCEPTED)
 
     def delete(self, request, format = None):
-        request_get = request.data
-        display_name = request_get.get('name')
-        user_id = request_get.get('user_id')
-        op = request_get.get('op')
+        print(request.data)
+        data = request.data
+        display_name = data.get('name')
+        user_id = data.get('user_id')
+        op = data.get('op')
 
         if display_name == None or user_id == None:
             raise exceptions.ValidationError(detail = None)
